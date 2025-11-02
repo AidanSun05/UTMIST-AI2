b6b572e2b2d51c252aa90d5ec0842479d25574ae
Aidan Sun <aidansun05@gmail.com>
2025-11-01 23:53:42 -0400

    Agent 5 ttnn

--- user/my_agent.py
+++ user/my_agent.py
@@ -27,6 +27,9 @@ import torch.nn.functional as F
 import gymnasium as gym
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
+# To run the sample TTNN model, you can uncomment the 2 lines below:
+import ttnn
+
 class MLPPolicy(nn.Module):
     def __init__(self, obs_dim: int = 64, action_dim: int = 10, hidden_dim: int = 64):
         super(MLPPolicy, self).__init__()
@@ -35,9 +38,9 @@ class MLPPolicy(nn.Module):
         self.fc3 = nn.Linear(hidden_dim, hidden_dim)
 
     def forward(self, obs):
-        x = F.relu(self.fc1(obs))
+        x = F.relu(self.fc1(obs.to(torch.float32)))
         x = F.relu(self.fc2(x))
-        return F.relu(self.fc3(x))
+        return F.relu(self.fc3(x)).to(torch.bfloat16)
 
 class MLPExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.Space, features_dim: int = 64, hidden_dim: int = 64):
@@ -58,6 +61,52 @@ class MLPExtractor(BaseFeaturesExtractor):
             features_extractor_kwargs=dict(features_dim=features_dim, hidden_dim=hidden_dim) #NOTE: features_dim = 10 to match action space output
         )
 
+class TTMLPPolicy(nn.Module):
+    def __init__(self, state_dict, mesh_device):
+        super(TTMLPPolicy, self).__init__()
+        self.mesh_device = mesh_device
+        self.fc1 = ttnn.from_torch(
+            state_dict["fc1.weight"].T,
+            device=mesh_device,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            layout=ttnn.TILE_LAYOUT,
+        )
+        self.fc2 = ttnn.from_torch(
+            state_dict["fc2.weight"].T,
+            device=mesh_device,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            layout=ttnn.TILE_LAYOUT,
+        )
+        self.fc3 = ttnn.from_torch(
+            state_dict["fc3.weight"].T,
+            device=mesh_device,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            layout=ttnn.TILE_LAYOUT,
+        )
+
+        self.fc1_b = ttnn.from_torch(state_dict["fc1.bias"], device=mesh_device, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        self.fc2_b = ttnn.from_torch(state_dict["fc2.bias"], device=mesh_device, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        self.fc3_b = ttnn.from_torch(state_dict["fc3.bias"], device=mesh_device, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+
+    def forward(self, obs: torch.Tensor) -> torch.Tensor:
+        # print("Running TT forward pass!")
+        obs = obs.to(torch.bfloat16)
+        tt_obs = ttnn.from_torch(obs, device=self.mesh_device, layout=ttnn.TILE_LAYOUT)
+
+        x1 = ttnn.linear(tt_obs, self.fc1, bias=self.fc1_b, activation="relu")
+        tt_obs.deallocate()
+
+        x2 = ttnn.linear(x1, self.fc2, bias=self.fc2_b, activation="relu")
+        x1.deallocate()
+
+        x3 = ttnn.linear(x2, self.fc3, bias=self.fc3_b, activation="relu")
+        x2.deallocate()
+
+        tt_out = ttnn.to_torch(x3).flatten().to(torch.float32)
+
+        return tt_out
+
+
 class SubmittedAgent(Agent):
     '''
     Input the **file_path** to your agent here for submission!
@@ -69,12 +118,24 @@ class SubmittedAgent(Agent):
         super().__init__(file_path)
         self.time = 0
 
+        # To run a TTNN model, you must maintain a pointer to the device and can be done by
+        # uncommmenting the line below to use the device pointer
+        self.mesh_device = ttnn.open_mesh_device(ttnn.MeshShape(1,1))
+
     def _initialize(self) -> None:
         if self.file_path is None:
             self.model = PPO("MlpPolicy", self.env, verbose=0)
             del self.env
         else:
             self.model = PPO.load(self.file_path, custom_objects={'policy_kwargs': MLPExtractor.get_policy_kwargs()})
+
+        # To run the sample TTNN model during inference, you can uncomment the 5 lines below:
+        # This assumes that your self.model.policy has the MLPPolicy architecture defined in `train_agent.py` or `my_agent_tt.py`
+        mlp_state_dict = self.model.policy.features_extractor.model.state_dict()
+        self.tt_model = TTMLPPolicy(mlp_state_dict, self.mesh_device)
+        self.model.policy.features_extractor.model = self.tt_model
+        self.model.policy.vf_features_extractor.model = self.tt_model
+        self.model.policy.pi_features_extractor.model = self.tt_model
 
     def _gdown(self) -> str:
         data_path = "rl-model.zip"
